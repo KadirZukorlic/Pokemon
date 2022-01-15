376899b7373ae77eea8cb3b1ac9798df9f05e62d
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-01-15 22:58:07 +0100

    showing spinner

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import './styles.scss';
+import { BounceLoader } from 'react-spinners';
+import { css } from '@emotion/react';
 
 import Button from './../Button/Button';
 import PokemonThumbnail from '../PokemonThumbnail/PokemonThumbnail';
 
+import './styles.scss';
+
+const loaderCSS = css`position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);`;
 
 const PokemonList = () => {
   const [pokemonList, setPokemonList] = useState([]);
@@ -11,47 +15,59 @@ const PokemonList = () => {
     'https://pokeapi.co/api/v2/pokemon?offset=20&limit=20'
   );
   const [isLoading, setIsLoading] = useState(false);
+  const [spinnerColor, setSpinnerColor] = useState('#ffffff');
 
   const getAllPokemons = async () => {
-    const url = ('https://pokeapi.co/api/v2/pokemon')
+    const url = 'https://pokeapi.co/api/v2/pokemon';
 
-    const res = await fetch(loadMore)
-    const data = await res.json()
+    setIsLoading(true);
 
-    setLoadMore(data.next)
+    const res = await fetch(loadMore);
+    const data = await res.json();
 
-    function configPokemonObject(results)  {
-      results.forEach( async pokemon => {
-        const res = await fetch(`${url}/${pokemon.name}`)
-        const data =  await res.json()
-        setPokemonList( currentList => [...currentList, data])
-        await pokemonList.sort((a, b) => a.id - b.id)
-      })
+    setLoadMore(data.next);
+
+    function configPokemonObject(results) {
+      results.forEach(async (pokemon) => {
+        const res = await fetch(`${url}/${pokemon.name}`);
+        const data = await res.json();
+        setPokemonList((currentList) => [...currentList, data]);
+        await pokemonList.sort((a, b) => a.id - b.id);
+      });
     }
-    configPokemonObject(data.results)
-  }
+    configPokemonObject(data.results);
+    setIsLoading(false);
+  };
 
- useEffect(() => {
-  getAllPokemons()
- }, [])
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
 
   return (
-    <div className="wrapper">
-      <div className="pokemon__wrapper">
-        <div className="pokemonList__wrapper">
-          {pokemonList.map( (pokemonStats, index) => 
-            <PokemonThumbnail
-              key={index}
-              id={pokemonStats.id}
-              image={pokemonStats.sprites.other.dream_world.front_default}
-              name={pokemonStats.name}
-              type={pokemonStats.types[0].type.name}
-            />)}
-          
-        </div>
+    <>
+      <BounceLoader
+        color={'#3b4cca'}
+        loading={isLoading}
+        size={150}
+        css={loaderCSS}
+      />
+      <div className="wrapper">
+        <div className="pokemon__wrapper">
+          <div className="pokemonList__wrapper">
+            {pokemonList.map((pokemonStats, index) => (
+              <PokemonThumbnail
+                key={index}
+                id={pokemonStats.id}
+                image={pokemonStats.sprites.other.dream_world.front_default}
+                name={pokemonStats.name}
+                type={pokemonStats.types[0].type.name}
+              />
+            ))}
+          </div>
           <Button onClick={() => getAllPokemons()}>Load more</Button>
+        </div>
       </div>
-    </div>
+    </>
   );
-}
+};
 export default PokemonList;
