08fbec8c5e647da1fd7542c9040f3a770b5264a2
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-01-18 03:05:02 +0100

    logo added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,24 +5,21 @@ import { useDispatch } from 'react-redux';
 import { searchTerm } from '../../redux/Search/searchActions';
 
 import FormInput from '../FormInput/FormInput';
-import Button from '../Button/Button';
-// import pokemonLogo from './../../assets/pokemonlogo.png';
+import pokeball from './../../assets/pokeball.png';
 
 const Header = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
+  return (
+    <div className="header">
+      <img src={pokeball} alt="pokemon logo" />
+      <FormInput
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => dispatch(searchTerm(event.target.value))}
+      />
+    </div>
+  );
+};
 
-
-    return (
-        <div className="header">
-            {/* <img src={pokemonLogo} alt="pokemon logo"/> */}
-            <FormInput 
-                type="text"
-                placeholder='Search...'
-                onChange={(event) => dispatch(searchTerm(event.target.value))}
-            />
-        </div>
-    )
-}
-
-export default Header
+export default Header;
